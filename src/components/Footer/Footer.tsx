c4cb311c57bb4fca93a01bd824471f65dede5510
tomaszek2115 <tomaush02@gmail.com>
2025-09-04 14:13:20 +0200

    footer implementation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,27 @@
 import "./Footer.css"
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      &copy; 2025 Audiomaster. Wszelkie prawa zastrzeżone.
+      <div className="footer-wrapper">
+        <div className="info">
+          <h3>Audiomaster Grzegorz Studencki</h3>
+          <p>+48 213 742 069</p>
+          <p>grzegorz.studencki@audiomaster.pl</p>
+          <p>warszawa ulica niebianskiego pokoju 88</p>
+        </div>
+        <div className="footer-icons">
+          <a href="https://www.instagram.com/audiomaster.pl/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="icon"/>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100057097914895" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className="icon"/>
+          </a>
+        </div>
+      </div>
+      <hr className="footer-separator" />
+      <p className="copyright">© 2025 Audiomaster.</p>
     </footer>
   );
 }
